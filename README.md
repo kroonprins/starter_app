e539b664150742b25c5885d67336be88fd665f88
Marco Debac <marco.debac@gmail.com>
2015-08-01 20:49:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<body>
+<p>Project template for quickly testing web developments with livereload using gulp</p>
+
+<p>Installation:</p>
+<ul>
+   <li>Clone from git</li>
+   <li>$ npm install</li>
+   <li>$ bower install</li>
+   <li>$ gulp</li>
+</ul>
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp');
+var EXPRESS_PORT = 8081;
+
+gulp.task('watch', function() {
+    var livereload = require('gulp-livereload');
+    livereload.listen();
+    gulp.watch(['js/*.js', '*.html', 'css/*.css']).on('change', livereload.changed);
+});
+
+gulp.task('server', function() {
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')());
+    app.use(express.static(__dirname));
+    app.listen(EXPRESS_PORT);
+});
+
+gulp.task('open', ['server'], function() {
+    var open = require('gulp-open');
+    var options = {
+      uri: 'http://localhost:'+EXPRESS_PORT
+    };
+    gulp.src(__filename)
+       .pipe(open(options));
+});
+
+gulp.task('default', ['open', 'watch'], function() {
+});
